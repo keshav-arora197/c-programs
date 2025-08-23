ba325a3575d5b2e75c80135146a2ed44e0ec15c3
keshav-arora197 <keshavarora778@gmail.com>
2025-08-23 15:18:10 +0530

    Added file1.c- sum of two numbers

---
+++ file1.c
@@ -0,0 +1,14 @@
+// Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main() {
+    int num1, num2, sum;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Ënter second number: ");
+    scanf("%d", &num2);
+    sum = num1+ num2;
+    printf("The sum is: %d\n", sum);
+    return 0;
+
+}
